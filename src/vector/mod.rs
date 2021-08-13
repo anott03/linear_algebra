18a7dea72b24da179b01a101aba31fef7be45b32
anott03 <amitav_nott@ryecountryday.org>
2021-08-13 09:59:13 -0400

    reorganization: - src/vector/mod.rs - src/vector/spec.rs

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -1,3 +1,5 @@
+mod spec;
+
 pub mod vector {
     pub fn dot_product(a: &Vec<i32>, b: &Vec<i32>) -> Result<i32, String> {
         if a.len() != b.len() {
@@ -75,63 +77,5 @@ pub mod vector {
         }
 
         return vector_addition(v, &_w)
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use crate::vector::vector;
-
-    #[test]
-    fn dot_prodcut_success() {
-        let v: Vec<i32> = vec![3, 1, 2];
-        let w: Vec<i32> = vec![1, 2, 3];
-        let result = vector::dot_product(&v, &w).unwrap();
-
-        assert_eq!(11, result);
-    }
-
-    #[test]
-    fn dot_prodcut_failure() {
-        let v: Vec<i32> = vec![3, 1, 2, 4];
-        let w: Vec<i32> = vec![1, 2, 3];
-
-        assert!(vector::dot_product(&v, &w).is_err());
-    }
-
-    #[test]
-    fn projection_success() {
-        let v: Vec<i32> = vec![1, -1, 1];
-        let w: Vec<i32> = vec![1, 1, 1];
-
-        let correct_projection: Vec<i32> = vec![2/3, 2/3, 2/3];
-        let calculated_projection = vector::projection(&v, &w).unwrap();
-
-        assert_eq!(correct_projection, calculated_projection);
-    }
-
-    #[test]
-    fn projection_failure() {
-        let v: Vec<i32> = vec![3, 1, 2, 4];
-        let w: Vec<i32> = vec![1, 2, 3];
-
-        assert!(vector::dot_product(&v, &w).is_err());
-    }
-
-    #[test]
-    fn projection_basis_success() {
-        let v = vec![3, 1, 2];
-        let basis = vec![vec![1, 1, 1], vec![1, -1, 0]];
-
-        let result = vector::projection_onto_basis(&v, &basis).unwrap();
-        assert_eq!(result, v);
-    }
-
-    #[test]
-    fn projection_basis_not_orthogonal() {
-        let v = vec![3, 1, 2];
-        let basis = vec![vec![1, 1, 1], vec![1, 1, 0]];
-        assert!(vector::projection_onto_basis(&v, &basis).is_err());
     }
 }
