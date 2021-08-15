40e705e0fa3384ab11607a393ad6ada885ecd37e
anott03 <amitav_nott@ryecountryday.org>
2021-08-15 15:21:51 -0400

    fraction stuff

--- src/fraction/mod.rs
+++ src/fraction/mod.rs
@@ -1,6 +1,7 @@
 mod spec;
 
 pub mod fraction {
+    #[derive(Debug)]
     pub struct Fraction {
         pub numerator: i32,
         pub denominator: i32
@@ -12,6 +13,27 @@ pub mod fraction {
                 numerator: n,
                 denominator: d
             }
+        }
+    }
+
+    impl PartialEq for Fraction {
+        fn eq(&self, other: &Self) -> bool {
+            return self.numerator == other.numerator &&
+                self.denominator == other.denominator;
+        }
+    }
+
+    pub fn multiply(a: &Fraction, b: &Fraction) -> Fraction {
+        return Fraction {
+            numerator: a.numerator * b.numerator,
+            denominator: a.denominator * b.denominator,
+        }
+    }
+
+    pub fn simplify(frac: &Fraction) -> Fraction {
+        return Fraction { // placeholder so rust analyzer doesn't get mad
+            numerator: 1,
+            denominator: 1,
         }
     }
 }
--- src/fraction/spec.rs
+++ src/fraction/spec.rs
@@ -7,4 +7,17 @@ mod tests {
         let f = fraction::Fraction::new(1, 3);
         assert!(1 == f.numerator && 3 == f.denominator);
     }
+
+    #[test]
+    fn multiply() {
+        let a = fraction::Fraction::new(1, 3);
+        let b = fraction::Fraction::new(1, 4);
+        let correct_answer = fraction::Fraction {
+            numerator: 1,
+            denominator: 12
+        };
+        let calculated_answer = fraction::multiply(&a, &b);
+
+        assert_eq!(correct_answer, calculated_answer);
+    }
 }
